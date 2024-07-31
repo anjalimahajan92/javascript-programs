a30d68e0fcc22def22b5a9df01dad02fda5fc4c7
anjalimahajan92 <anjalimahajan2000@gmail.com>
2024-07-31 16:18:25 +0530

    02-typeOf
    02-typeOf

---
+++ 02-typeOf.js
@@ -0,0 +1,33 @@
+// ===============1=======//
+console.log("======1=====");
+  var bankName;
+  console.log("Bank name is  before initionalization",bankName);
+  var bankName="SBI Bank";
+  var bankNameTypeOf=typeof bankName;
+console.log("Type of bank name is after initionalization",bankNameTypeOf);
+// ==============2=========//
+console.log("=======2======");
+var marks="90%";
+console.log("Marks is",marks);
+var marksTypeOf=typeof marks;
+console.log("Type of marks is",marksTypeOf);
+// ==============3===========//
+console.log("======3=====");
+var isWorking;
+console.log("is Working berore initionlization",isWorking);
+var isWorking= true ;
+console.log("isWorking ",isWorking);
+var isWorkingTypeOf=typeof isWorking;
+console.log("Data type is",isWorkingTypeOf);
+// =================4 (a)=========//
+console.log("========4(a)=======");
+var totalCount="Hundred and seven";
+console.log("total count is",totalCount);
+var totalCountTypeOf=typeof totalCount;
+console.log("totalcount data type is",totalCountTypeOf);
+// ================4(b)==============//
+console.log("======4(b)=====");
+var totalCount=107;
+console.log("totalCount is",totalCount);
+var totalCountTypeOf=typeof totalCount;
+console.log("totalcount data type is",totalCountTypeOf);
