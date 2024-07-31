b166d4c05c30df110a4a031d6b30564d5455d321
anjalimahajan92 <anjalimahajan2000@gmail.com>
2024-07-31 16:13:45 +0530

    01-variableAssign
    01-variableAssign

---
+++ 01-variableAssign.js
@@ -0,0 +1,28 @@
+// ====1====//
+console.log("=========1=======");
+var myFullName;
+console.log("Variable before initionlization",myFullName);
+var myFullName="Anjali mahajan";
+console.log("My first name and last name after initionlization is",myFullName);
+var myFullName="Anjali Ashok Mahajan";
+console.log("My first name ,middle name and last name after updating full name is",myFullName);
+// ======2=====//
+console.log("=======2=======");
+var myLove="I love only JS";
+console.log(myLove);
+// =======3======//
+console.log("=========3=======");
+var firstName="anjali"
+console.log("My first name is",firstName);
+var lastname="Mahajan";
+console.log("My last name is",lastname);
+var age=24;
+console.log("My age is",age);
+var address=" My address is Mothe waghode, tal-raver, Dist-jalgaon";
+console.log(address);
+var pincode=425502;
+console.log("pinCode is",pincode);
+var gender="Female";
+console.log("gender is",gender);
+var isMarried="yes";
+console.log("ismarried",isMarried);
