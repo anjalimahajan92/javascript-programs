3f9f60313bd9d2092fa9126cd37bba1f29299a3b
anjalimahajan92 <anjalimahajan2000@gmail.com>
2024-08-02 16:48:09 +0530

    03-funAssingnment -02
    03-funAssingnment -02

---
+++ 03-funAssig02.js
@@ -0,0 +1,7 @@
+function bankDetails(bankName,accountNum,location,pinCode)
+{
+     console.log("Bank name is:",bankName,"||","Account number is:",accountNum,"||","Location is:",location,"||","pinCode is:",pinCode);
+} 
+bankDetails("CITY Bank",3456782345,"Pune",431202);
+bankDetails("Axis Bank",7856782345,"Mumbai",441202);
+bankDetails("HDFC Bank",8956782345,"Pune",631202,"Open");
