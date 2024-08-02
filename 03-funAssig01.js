8290a62037f80415a5b0548d661913085be5c9c7
anjalimahajan92 <anjalimahajan2000@gmail.com>
2024-08-02 16:38:36 +0530

    03-Function in js Assignment -01
    03-Function in js Assignment -01

---
+++ 03-funAssig01.js
@@ -0,0 +1,34 @@
+console.log("===============================1================================");
+
+function show()
+{
+    console.log("I am interested in JavaScript");
+    
+}
+show();
+function div()
+{
+    console.log("I am also interested in angular and react");
+    
+}
+div();
+console.log("============================2============================");
+function personalDetails(firstName, lastName ,collegeName)
+{
+ 
+console.log("My first name is ",firstName);
+
+console.log("My last name is ",lastName);
+
+console.log("My college name is",collegeName);
+} 
+personalDetails("Anjali","Mahajan","J T Mahajan ");
+console.log("===================================3========================");
+function addThreeValue(n1,n2,n3){
+    var result=n1+n2+n3;
+    console.log("Result is",result);
+    
+}
+addThreeValue(10.23,600,40);
+addThreeValue(" Hello "," Good "," Morning");
+
