09fbc30b3bd057457a3e837cc7d1041b6a6d6157
anjalimahajan92 <anjalimahajan2000@gmail.com>
2024-08-07 15:35:35 +0530

    05-implicit conversion
    05-implicit conversion

---
+++ 05-implicit_conversion.js
@@ -0,0 +1,11 @@
+result = '3' + 2;
+console.log(`Result is:${result}`);
+
+result = '3' + true;
+console.log(`Result is:${result}`);
+
+result = '3' + undefined;
+console.log(`Result is:${result}`);
+
+result = '3' + null;
+console.log(`Result is:${result}`);
