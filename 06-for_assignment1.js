fc4154c46c5ac6b18254396344c69771abc6c018
anjalimahajan92 <anjalimahajan2000@gmail.com>
2024-08-10 18:15:42 +0530

    06-for_assignment
    06-for_assignment

---
+++ 06-for_assignment1.js
@@ -0,0 +1,59 @@
+
+console.log("=====1)WAP program to print  5 to 15 ==========");
+
+
+for (let index = 5; index <= 15; index++) {
+   console.log(index);
+
+
+}
+
+console.log("===== 2)WAP program to print 50 to 40 ==========");
+
+for (let index = 50; index >= 40; index--) {
+   console.log(index);
+
+
+
+}
+
+console.log("=====3)WAP program to find 15 odd number==========");
+
+for (let index = 1; index <= 30; index = index + 2) {
+   console.log(index);
+
+
+
+}
+
+console.log("=====4)WAP program to find 10 Even number==========");
+
+for (let index = 2; index <= 21; index = index + 2) {
+   console.log(index);
+
+
+}
+
+console.log("=====5)WAP program to print table of 5==========");
+
+for (let index = 5; index <= 50; index = index + 5) {
+   console.log(index);
+
+
+}
+
+console.log("=====6)WAP program to table  of 10=========");
+
+for (let index = 10; index <= 100; index = index + 10) {
+   console.log(index);
+
+
+}
+
+console.log("=====7)WAP program to table  of 10 in reverse order=========");
+
+for (let index = 100; index>=10;index = index -10 ) {
+   console.log(index);
+
+
+}
