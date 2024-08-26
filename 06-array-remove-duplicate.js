ebd67971f042e8e8eb4f869c69b3354855000445
anjalimahajan92 <anjalimahajan2000@gmail.com>
2024-08-26 20:44:56 +0530

    06-array-remove-duplicate
    06-array-remove-duplicate

---
+++ 06-array-remove-duplicate.js
@@ -0,0 +1,16 @@
+console.log("============remove duplicate element in array=====");
+
+
+const arrayNum=[11,3,4,11,4,7,3];
+
+for (const element of arrayNum) {
+   console.log(arrayNum);
+ let newSet = [...new Set(arrayNum)];
+console.log(newSet);
+
+ break;
+}
+
+
+
+
