b6533845fb84127cee042a2851ec240dfa62490d
anjalimahajan92 <anjalimahajan2000@gmail.com>
2024-08-26 20:33:02 +0530

    06-prime-number-count
    06-prime-number-count

--- 06-prime-number-count.js
+++ 06-prime-number-count.js
@@ -25,3 +25,53 @@ function isPrime(num) {
     return true;
 }
 checkPrime([3, 9, 7, 6, 19, 29, 53]);
+
+console.log("=====11)space count in string====");
+
+function spaceCount(key) {
+    console.log(key);
+    let counter = 0;
+    for (let index = 0; index < key.length; index++) {
+
+        let keyChar = key.charAt(index);
+        if (keyChar == " ") {
+            counter++;
+        }
+
+    }
+    return counter;
+}
+let spaceCount1 = spaceCount("Revision is the mother of success");
+console.log(`total Spaces are:${spaceCount1}`);
+
+let spaceCount2 = spaceCount("JavaScript is the language of internet world");
+console.log(`total Spaces are:${spaceCount2}`);
+
+console.log("===========10  =====");
+function duplicate(params) {
+let mainOp={};
+ for (let index = 0; index < params.length; index++) {
+    const elements = params.charAt(index);
+    let count=0;
+    for (let j =0; j < params.length;j++) {
+  let char= params.charAt(j)
+        if(char==elements){
+            count++;
+        }
+    }
+    if(count>1){
+        mainOp[elements]=count;
+    }
+
+ }
+for (const key in mainOp) {
+   
+        const result = mainOp[key];
+        console.log(`${key}-${result}`);
+        
+    }
+}
+    
+duplicate("Banana");
+duplicate("Apple");
+duplicate("Software Developer");
